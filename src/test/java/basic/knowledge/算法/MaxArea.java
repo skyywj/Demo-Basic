02690c348ea27f743bfe547e21d705de706cf25d
Yan Wenjie <yanwenjie@zidanduanxin.com>
2018-11-03 21:49:27 +0800

    添加大数相加相乘算法以及最大容量算法

---
+++ src/test/java/basic/knowledge/算法/MaxArea.java
@@ -0,0 +1,34 @@
+package basic.knowledge.算法;
+
+/**
+ * @author yanwenjie
+ * @since 2018/11/2
+ * desc: 计算哪个容器可以盛最多水
+ * code地址:   https://leetcode-cn.com/problems/container-with-most-water/
+ */
+public class MaxArea {
+
+    public static int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
+        int l = i;
+        int r = j;
+        while (i < j) {
+            int h = height[i] > height[j] ? height[j] : height[i];
+            max = h * (j - i) > max ? h * (j - i) : max;
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String... args) {
+        int a[] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int res = maxArea(a);
+        System.out.print(res);
+    }
+}
---
+++ src/test/java/basic/knowledge/算法/MaxNumAddAndMultiply.java
@@ -0,0 +1,109 @@
+package basic.knowledge.算法;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: CarryJey @Date: 2018/10/24 21:03:53
+ * desc:大数相乘 & 大数相加
+ */
+public class MaxNumAddAndMultiply {
+    /**
+     * 乘可以拆分为两步：先单个乘然后每次多增一个0，在挨个相加。
+     */
+    public static String multiply(String num1, String num2) {
+        if (num1.length() == 1 && Integer.valueOf(num1) == 0) {
+            return "0";
+        }
+        if (num2.length() == 1 && Integer.valueOf(num2) == 0) {
+            return "0";
+        }
+        num1 = new StringBuilder(num1).reverse().toString();
+        num2 = new StringBuilder(num2).reverse().toString();
+        int t = 0;
+        int i = 0;
+        String res = "";
+        List<String> listResult = new ArrayList<>();
+        int len = num1.length() > num2.length() ? num1.length() : num2.length();
+        while (i < num2.length()) {
+            int y = num2.charAt(i) - '0';
+            String midResult = "";
+            /**
+             * 加0操作
+             */
+            for (int k = 0; k < i; k++) {
+                midResult += String.valueOf(0);
+            }
+            for (int j = 0; j < num1.length(); j++) {
+                int x = num1.charAt(j) - '0';
+                int temp = x * y + t;
+                midResult += temp % 10;
+                t = temp / 10;
+                if (j + 1 == num1.length() && t > 0) {
+                    midResult += t;
+                    t = 0;
+                }
+            }
+            listResult.add(new StringBuilder(midResult).reverse().toString());
+            i++;
+        }
+
+        /**
+         * 相加操作
+         */
+        for (int k = 0; k < listResult.size(); k++) {
+            res = add(res, listResult.get(k));
+        }
+        return res;
+    }
+
+    /**
+     * 大数相加
+     */
+    public static String add(String num1, String num2) {
+        num1 = new StringBuilder(num1).reverse().toString();
+        num2 = new StringBuilder(num2).reverse().toString();
+        int t = 0;
+        int i = 0;
+        String res = "";
+        while (i < num1.length() && i < num2.length()) {
+            int x = num1.charAt(i) - '0';
+            int y = num2.charAt(i) - '0';
+            int sum = x + y + t;
+            res += String.valueOf(sum % 10);
+            t = sum / 10;
+            i++;
+        }
+
+        while (i < num1.length()) {
+            if (t > 0) {
+                Integer r = num1.charAt(i) - '0' + t;
+                t = r / 10;
+                res += String.valueOf(r % 10);
+            } else {
+                res += num1.charAt(i) - '0';
+            }
+            i++;
+        }
+
+        while (i < num2.length()) {
+            if (t > 0) {
+                Integer r = num2.charAt(i) - '0' + t;
+                t = r / 10;
+                res += String.valueOf(r % 10);
+            }else {
+                res+=num2.charAt(i)-'0';
+            }
+            i++;
+        }
+        if (t > 0) {
+            res += String.valueOf(t);
+        }
+        return new StringBuilder(res).reverse().toString();
+    }
+
+    public static void main(String... args) {
+        String res = multiply("123456789", "98765456451");
+        System.out.print(res);
+    }
+}
